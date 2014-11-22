d6a60b13e563ef6f1c0d217a63ef396f39baf5e4
Osinko <yoshinari_m@msn.com>
2014-11-23 07:28:12 +0900

    no message

---
+++ Assets/Line2LineCheck.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Line2LineCheck : MonoBehaviour
+{
+		public Vector3 vStart, vEnd;
+		public Vector3 wStart, wEnd;
+		public bool line2LineCheck;
+
+		void Update ()
+		{
+				line2LineCheck = CheckLine2Line (vStart, vEnd, wStart, wEnd);
+		}
+
+		float t;
+		Vector3 test;
+
+		public bool CheckLine2Line (Vector3 vStart, Vector3 vEnd, Vector3 wStart, Vector3 wEnd, bool segments=false)
+		{
+				Vector3 v = vEnd - vStart;
+				Vector3 w = wEnd - wStart;
+				Vector3 common = wEnd - vEnd;
+
+				Vector3 cross_vw = Vector3.Cross (v, w);
+				Vector3 cross_common = Vector3.Cross (common, v);
+				bool crossNormalCheck = 0 == (Vector3.Cross (cross_vw, cross_common)).sqrMagnitude;
+	
+				t = cross_common.magnitude / cross_vw.magnitude;
+				test = w * -t;
+
+				Debug.DrawLine (vEnd, vEnd + common, Color.grey);
+				Debug.DrawLine (Vector3.zero, cross_vw, Color.white);
+				Debug.DrawLine (Vector3.zero, cross_common, Color.grey);
+				Debug.DrawLine (vStart, vEnd, Color.green);
+				Debug.DrawLine (wStart, wEnd, Color.blue);
+				Debug.DrawLine (wEnd, wEnd + test, Color.red);
+		
+				return crossNormalCheck;
+		}
+
+}
--- Assets/ShortLine1.cs
+++ Assets/ShortLine1.cs
@@ -3,16 +3,17 @@ using System.Collections;
 
 public class ShortLine1 : MonoBehaviour
 {
-		public Vector3 v, w;
-		public Vector3 h;
+	public Vector3 v, w;
+	public Vector3 horizontal;
 
-		void Update ()
-		{
-				Vector3 wn = w.normalized;
-				h = Vector3.Dot (v, wn) * wn; 
+	void Update ()
+	{
+		Vector3 wn = w.normalized;
+		horizontal = Vector3.Dot (v, wn) * wn; 
 
-				Debug.DrawLine (Vector3.zero, v, Color.blue);
-				Debug.DrawLine (Vector3.zero, w, Color.green);
-				Debug.DrawLine (v, h, Color.red);
-		}
+		Debug.DrawLine (Vector3.zero, v, Color.blue);
+		Debug.DrawLine (Vector3.zero, w, Color.green);
+		Debug.DrawLine (v, horizontal, Color.red);
+
+	}
 }
