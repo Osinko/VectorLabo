660b9f42d03d8e6c70f793cccee1d21b8a4fc146
Osinko <yoshinari_m@msn.com>
2014-11-26 02:22:38 +0900

    no message

--- Assets/Line2LineCheck2.cs
+++ Assets/Line2LineCheck2.cs
@@ -8,17 +8,14 @@ public class Line2LineCheck2 : MonoBehaviour
 		public  Vector3 vStart, vEnd, wStart, wEnd;
 		public bool crossCheck;
 
-		Vector3 crossPoint;
+		Vector3 crossPoint, crossPoint2;
 		Vector3 p1, p2, p3, p4;
-
+	
 		float t1, t2;
 		Vector3 newVector, newVector2;
 		bool crossNormalCheck, lengthCheck, insideCheck;
 
 		Vector3 cross_vw, cross_common, cross_common2;
-		Vector3 incheck, incheck2;
-
-		Vector3 vStartWend, vEndWstart;
 
 		void Update ()
 		{
@@ -40,24 +37,29 @@ public class Line2LineCheck2 : MonoBehaviour
 				newVector2 = v * t2;
 
 				crossPoint = newVector + wEnd;	//交点
+				crossPoint2 = newVector2 + vStart;	//交点
 
-				//各辺のベクトルと交点に対する外積を取って内側にある確認する
-				p1 = Vector3.Cross (vStart - wStart, vStart - crossPoint);
-				p2 = Vector3.Cross (wEnd - vStart, wEnd - crossPoint);
-				p3 = Vector3.Cross (vEnd - wEnd, vEnd - crossPoint);
-				p4 = Vector3.Cross (wStart - vEnd, wStart - crossPoint);
+				//各辺のベクトルと交点に対する外積を取って交点が線同士の内側にあるか確認する
+				p1 = Vector3.Cross (vStart - wStart, crossPoint - vStart);
+				p2 = Vector3.Cross (wEnd - vStart, crossPoint2 - wEnd);
+				p3 = Vector3.Cross (vEnd - wEnd, crossPoint - vEnd);
+				p4 = Vector3.Cross (wStart - vEnd, crossPoint2 - wStart);
 
-//				incheck = Vector3.Cross (vStartWend, newVector);
-//				incheck2 = Vector3.Cross (vStartWend, newVector2);
-//				insideCheck = incheck.x * incheck2.x <= 0 && incheck.y * incheck2.y <= 0 && incheck.z * incheck2.z <= 0;
+				bool p1Check = p1.x * cross_vw.x <= 0 && p1.y * cross_vw.y <= 0 && p1.z * cross_vw.z <= 0;
+				bool p2Check = p2.x * cross_vw.x <= 0 && p2.y * cross_vw.y <= 0 && p2.z * cross_vw.z <= 0;
+				bool p3Check = p3.x * cross_vw.x <= 0 && p3.y * cross_vw.y <= 0 && p3.z * cross_vw.z <= 0;
+				bool p4Check = p4.x * cross_vw.x <= 0 && p4.y * cross_vw.y <= 0 && p4.z * cross_vw.z <= 0;
+
+				insideCheck = p1Check && p2Check && p3Check && p4Check;
 
 				crossNormalCheck = epsilon > (Vector3.Cross (cross_common, cross_vw)).sqrMagnitude;
 				lengthCheck = t1 + epsilon > 0 && t1 - epsilon < 1 && t2 + epsilon > 0 && t2 - epsilon < 1;
 
-				if (crossNormalCheck && lengthCheck) {
+				if (crossNormalCheck && lengthCheck && insideCheck) {
 						crossCheck = true;
 				}
 
+				//確認用コード
 				Debug.DrawLine (vStart, vStart + v, Color.blue);
 				Debug.DrawLine (wStart, wStart + w, Color.green);
 				Debug.DrawLine (wEnd, newVector + wEnd, Color.red);
@@ -68,6 +70,5 @@ public class Line2LineCheck2 : MonoBehaviour
 				Debug.DrawLine (Vector3.zero, cross_common, Color.cyan);
 				Debug.DrawLine (Vector3.zero, cross_common2, Color.yellow);
 				Debug.DrawLine (Vector3.zero, cross_vw, Color.red);
-	
 		}
 }
